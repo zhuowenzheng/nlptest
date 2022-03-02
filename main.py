3758be0abe760e7d254454a700803a02ad251877
alexzheng <alexzheng@tongji.edu.cn>
2022-03-02 16:17:39 +0800

    fixed small syntax errors

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-def Forward_MM(user_dict, sentence):
+def forward_mm(user_dict, sentence):
     '''
     :param user_dict:
     :param sentence:
@@ -24,7 +24,7 @@ def Forward_MM(user_dict, sentence):
     return result
 
 
-def Backward_MM(user_dict, sentence):
+def backward_mm(user_dict, sentence):
     '''
     :param user_dict:
     :param sentence:
@@ -45,43 +45,43 @@ def Backward_MM(user_dict, sentence):
             index += 1
     return result
 
-def Bidirectional_MM(user_dict,sentence):
+def bidirectional_mm(user_dict,sentence):
     '''
     :param user_dict:
     :param sentence:
     :return: result of bidirectional max match, print the result
     '''
-    FMM_result = Forward_MM(user_dict, sentence)
-    BMM_result = Backward_MM(user_dict, sentence)
-    fmm_max_len = max([len(item) for item in FMM_result])
-    bmm_max_len = max([len(item) for item in BMM_result])
-    fmm_num_single = np.sum([len(item)==1 for item in FMM_result])
-    bmm_num_single = np.sum([len(item)==1 for item in BMM_result])
+    fmm_result = forward_mm(user_dict, sentence)
+    bmm_result = backward_mm(user_dict, sentence)
+    fmm_max_len = max([len(item) for item in fmm_result])
+    bmm_max_len = max([len(item) for item in bmm_result])
+    fmm_num_single = np.sum([len(item)==1 for item in fmm_result])
+    bmm_num_single = np.sum([len(item)==1 for item in bmm_result])
     # Rules: 比较fmm和bmm的分词结果，取所得分词中单个词最长的优先，若两者相同，则取单个词最少的
     if(fmm_max_len>bmm_max_len):
         print('\n双向最大匹配结果：')
-        for item in FMM_result:
+        for item in fmm_result:
             print(item, end='/')
     elif(fmm_max_len<bmm_max_len):
         print('\n双向最大匹配结果：')
-        for item in BMM_result[::-1]:
+        for item in bmm_result[::-1]:
             print(item, end='/')
     elif(fmm_max_len==bmm_max_len):
         if(fmm_num_single>bmm_num_single):
             print('\n双向最大匹配结果：')
-            for item in FMM_result:
+            for item in fmm_result:
                 print(item, end='/')
         else:
             print('\n双向最大匹配结果：')
-            for item in BMM_result[::-1]:
+            for item in bmm_result[::-1]:
                 print(item, end='/')
 
 def main():
     user_dict = ['我们', '在', '在野', '生动', '野生', '动物园', '野生动物园', '物', '园', '玩']
     sentence = '我们在野生动物园玩'
-    #Forward_MM(user_dict, sentence)
-    #Backward_MM(user_dict, sentence)
-    Bidirectional_MM(user_dict, sentence)
+    #forward_mm(user_dict, sentence)
+    #backward_mm(user_dict, sentence)
+    bidirectional_mm(user_dict, sentence)
 
 if __name__ == '__main__':
     main()
